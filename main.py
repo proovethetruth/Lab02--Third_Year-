fe054f1ddc93ab3d8d9b489f4bda30f4847548ad
seeker <populpock2@gmail.com>
2022-11-19 00:30:57 +0300

    Edited

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from logging import exception
 import time
-from threading import Thread
-from threading import Lock
 
+import numpy as np
 import threading
 from numpy import random
 from PyQt5.QtCore import Qt
@@ -13,79 +12,7 @@ from PyQt5.QtGui import QIcon
 # - - - - - - - - - - - - - - LOGGING - - - - - - - - - - - - - -  # 
 
 
-
-# - - - - - - - - - - - - - - THREADING - - - - - - - - - - - - - -  # 
-
-def threading_gen(arr):
-    global arr2
-    arr2 = arr
-    start = time.perf_counter()
-    th1 = threading.Thread(target = first_thread, args = arr2)
-    th2 = threading.Thread(target = second_thread, args = arr2)
-    th1.start()
-    th2.start()
-    th1.join()
-    th2.join()
-    th3 = threading.Thread(target = third_thread, args = arr2)
-    th3.start()
-    th3.join()
-    t_lbl2.setText('Время многопоточной обработки:' + str(time.perf_counter() - start))
-    return arr2
-
-
-def first_thread(arr):
-    lock = Lock()
-    tmp = arr
-    
-    for i in range(len(arr)):
-        if i > 1 and i < 12:
-            tmp[i] = -(tmp[i]**2)
-        else:
-            tmp[i] = tmp[i] - 1
-    lock.acquire()
-    arr2 = tmp
-    lock.release()
-
-def second_thread(arr):
-    tmp = arr
-    maxim = -9999
-    max_i = 0
-    for i in range(len(arr)):
-        if i > 1 and i < 12:
-            if -(arr[i]**2) > maxim:
-                maxim = -(arr[i]**2)
-                max_i = i
-        else:
-            if (arr[i] - 1) > maxim:
-                maxim = arr[i] - 1
-                max_i = i
-    arr2[max_i] = maxim
-
-def third_thread(arr):
-    max1 = max(arr)
-    arr[arr.index(max1)] = -max1
-    if arr.index(max(arr)) + 1 < arr.index(min(arr)):
-        for i in range(arr.index(max(arr)) + 1, arr.index(min(arr))):
-            arr[i] = 0
-    else:
-        for i in range(arr.index(min(arr)) + 1, arr.index(max(arr))):
-            arr[i] = 0
-    return arr
-
-
-
 # - - - - - - - - - - - - - ARRAY FUNCTIONS - - - - - - - - - - - - - # 
-
-def array_gen(arr):
-    start = time.perf_counter()
-    resultArray = []
-    for i in range(len(arr)):
-        if arr[i] % 3 == 0 and arr[i] % 5 == 0 and arr[i] != 0:
-            resultArray.append(i + 1)
-
-    t_lbl1.setText('Время однопоточной обработки:' + str(time.perf_counter() - start))
-    return resultArray
-
 
 def get_arr():
     arr = []
@@ -107,12 +34,13 @@ def resize():
 
 def fill():
     arr = [0 for i in range (arrays_table.columnCount())]
+
     if manualInput.isChecked():
         arr = get_arr()
     elif randomInput.isChecked():
-        arr = random.random_integers(1, 100, arrays_table.columnCount())
+        arr = random.random_integers(1, 25, arrays_table.columnCount())
     elif randomPeriodInput.isChecked():
-        temp_arr = random.random_integers(1, 100, 4)
+        temp_arr = random.random_integers(1, 25, 4)
         for i in range(arrays_table.columnCount()):
             arr[i] = temp_arr[i % 4]
 
@@ -122,19 +50,17 @@ def fill():
             item.setFlags( Qt.ItemIsSelectable | Qt.ItemIsEnabled )
             arrays_table.setItem(0, i, item)
 
-    resultArray = array_gen(arr)
-    # threading_gen(arr)
+    start = time.perf_counter()
+    solve_threads(arr)
+    t_lbl2.setText('Время многопоточной обработки: ' + str(time.perf_counter() - start))
 
-    if len(resultArray) == 0:
-        arrays_table.setItem(1, 0, QTableWidgetItem("None"))
-    else:
-        for i in range(len(resultArray)):
-            item2 = QTableWidgetItem(str(resultArray[i]))
-            item2.setFlags( Qt.ItemIsSelectable | Qt.ItemIsEnabled )
-            arrays_table.setItem(1, i, item2)
+    solve_first_task(arr)
+    solve_second_task(arr)
+    solve_third_task(arr)
 
-        for i in range(arrays_table.columnCount() - len(resultArray)):
-            arrays_table.setItem(1, i + len(resultArray), QTableWidgetItem(""))
+
+
+# - - - - - - - - - - - -  TABLE EDITING PERMISSIONS - - - - - - - - - - - - # 
 
 def table_dis():
     for i in range(arrays_table.columnCount()):
@@ -155,13 +81,85 @@ def table_en():
 
 
 
+# - - - - - - - - - - - - - - THREADING - - - - - - - - - - - - - -  # 
+
+def solve_threads(arr):
+    global thread_result_array
+    thread_result_array = []
+
+    thread_list = []
+    numpy_array = np.array_split(arr, 4)
+    for i in range(4):
+        thread_list.append(threading.Thread(target = thread_worker, args = (numpy_array[i], i)))
+        thread_list[i].start()
+
+    for i in range(len(thread_list)):
+        thread_list[i].join()
+    print(thread_result_array)
+
+
+def thread_worker(arr, i):
+    for i in range(len(arr)):
+        if arr[i] % 3 == 0 and arr[i] % 5 == 0 and arr[i] != 0:
+            thread_result_array.append(i + 1)
+
+
+# - - - - - - - - - - - - - SOLVING TASKS - - - - - - - - - - - - - # 
+
+def solve_first_task(arr):
+    start = time.perf_counter()
+    
+    resultArray = []
+    for i in range(len(arr)):
+        if arr[i] % 3 == 0 and arr[i] % 5 == 0 and arr[i] != 0:
+            resultArray.append(i + 1)
+
+    t_lbl1.setText('Время однопоточной обработки: ' + str(time.perf_counter() - start))
+
+    if len(resultArray) == 0:
+        arrays_table.setItem(1, 0, QTableWidgetItem("None"))
+        for i in range(arrays_table.columnCount() - 1):
+            arrays_table.setItem(1, i + 1, QTableWidgetItem(""))
+    else:
+        for i in range(len(resultArray)):
+            item2 = QTableWidgetItem(str(resultArray[i]))
+            item2.setFlags( Qt.ItemIsSelectable | Qt.ItemIsEnabled )
+            arrays_table.setItem(1, i, item2)
+
+        for i in range(arrays_table.columnCount() - len(resultArray)):
+            arrays_table.setItem(1, i + len(resultArray), QTableWidgetItem(""))
+ 
+
+def solve_second_task(arr):
+    result = 0
+    for i in range(len(arr)):
+        if arr[i] < 10:
+            result += arr[i]
+    task2.setText('Сумма положительных элементов, значения которых меньше 10: ' + str(result))
+
+
+def solve_third_task(arr):
+    result = False
+    try:
+        num = int(num_edit.text())
+    except:
+        exception('Введено не число')
+        task3.setText('Есть ли пара соседних элементов с суммой, равной заданному числу? Число не задано')
+        return
+
+    for i in range(len(arr) - 1):
+        if arr[i] + arr[i + 1] == num:
+            result = True
+    task3.setText('Есть ли пара соседних элементов с суммой, равной заданному числу? ' + ''.join("Да" if result == True else "Нет"))
+
+
 # - - - - - - - - - - - - - - - MAIN - - - - - - - - - - - - - - - # 
 
 app = QApplication([])
 app.setApplicationName("Lab02 (3rd year)")
 app.setWindowIcon(QIcon('C:\\Users\\asus\\Desktop\\Projects\\SUAI\\Labs\\3-ий курс\\C++\\Lab02 Python\\plex.jpg'))
 window = QWidget()
-window.setGeometry(100, 100, 900, 320)
+window.setGeometry(100, 100, 900, 450)
 window.show()
 
 grid1 = QGridLayout()
@@ -171,14 +169,20 @@ lbl = QLabel('Введите длину массива:')
 grid1.addWidget(lbl, 0, 0)
 len_edit = QLineEdit()
 grid1.addWidget(len_edit, 1, 0)
+
+num_lbl = QLabel('Введите число:')
+grid1.addWidget(num_lbl, 2, 0)
+num_edit = QLineEdit()
+grid1.addWidget(num_edit, 3, 0)
+
+
 btn_resize = QPushButton('Ввод')
 btn_resize.clicked.connect(resize)
 grid1.addWidget(btn_resize, 1, 1)
 
-
 btn_fill = QPushButton('Заполнить')
 btn_fill.clicked.connect(fill)
-grid1.addWidget(btn_fill, 2, 0)
+grid1.addWidget(btn_fill, 4, 3)
 
 lbl2 = QLabel('Способ заполнения массива:')
 grid1.addWidget(lbl2, 0, 3)
@@ -203,20 +207,20 @@ for i in range(arrays_table.columnCount()):
     item = QTableWidgetItem()
     item.setFlags( Qt.ItemIsSelectable | Qt.ItemIsEnabled )
     arrays_table.setItem(1, i, item)
-grid1.addWidget(arrays_table, 4, 0, 1, 4)
+grid1.addWidget(arrays_table, 5, 0, 1, 4)
 
 
-t_lbl1 = QLabel('Время однопоточной обработки:')
-grid1.addWidget(t_lbl1, 5, 0)
+t_lbl1 = QLabel('Время однопоточной обработки: ')
+grid1.addWidget(t_lbl1, 6, 0)
 
-t_lbl2 = QLabel('Время многопоточной обработки:')
-grid1.addWidget(t_lbl2, 5, 2)
+t_lbl2 = QLabel('Время многопоточной обработки: ')
+grid1.addWidget(t_lbl2, 6, 2)
 
-t_lbl2 = QLabel('Сумма положительных элементов, значения которых меньше 10:')
-grid1.addWidget(t_lbl2, 6, 0)
+task2 = QLabel('Сумма положительных элементов, значения которых меньше 10: ')
+grid1.addWidget(task2, 7, 0)
 
-t_lbl2 = QLabel('Есть ли пара соседних элементов с суммой, равной заданному числу?')
-grid1.addWidget(t_lbl2, 7, 0)
+task3 = QLabel('Есть ли пара соседних элементов с суммой, равной заданному числу? ')
+grid1.addWidget(task3, 8, 0)
 
 table_dis()
 
